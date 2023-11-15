512c04906e70f58432936a77ad130ce9b17070f8
Trương Công Phi <tcp02012003@gmail.com>
2023-11-15 23:30:47 +0700

    fix khoa hoc

--- src/khoa_hoc.php
+++ src/khoa_hoc.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Khóa học</title>
-	<!-- Begin bootstrap cdn -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="	sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-	<!-- End bootstrap cdn -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Khóa học</title>
+    <!-- Begin bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="	sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <!-- End bootstrap cdn -->
 
 </head>
-<body>
-	<?php include 'navbar.php';?>
-	<main style="min-height: 100vh; width: 100%;">
-		<div class="" style="text-align: center;">
-			<h2>Khóa học</h2>
-		</div>
-		<div class="row row-cols-1 row-cols-md-3 g-4" style="margin: 0 auto; width: 80%;">
-			<!-- begin khóa học -->
 
-			<?php
+<body>
+    <?php include 'navbar.php';?>
+    <main style="min-height: 100vh; width: 100%;">
+        <div class="" style="text-align: center;">
+            <h2>Khóa học</h2>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4" style="margin: 0 auto; width: 80%;">
+            <!-- begin khóa học -->
+
+            <?php
 			include '../connectdb.php';
 			
 			
-			// $username = $_SESSION['username'];
+			$username = $_SESSION['username'];
 
 			$query = "SELECT * FROM khoa_hoc";
 			$result = mysqli_query($conn, $query);
@@ -68,12 +73,12 @@
 
 
 
-			<!-- end khóa học -->
-			
+            <!-- end khóa học -->
 
-		</div>
-	</main>
-	<?php include 'footer.php'; ?>
+
+        </div>
+    </main>
+    <?php include 'footer.php'; ?>
 </body>
 
 
