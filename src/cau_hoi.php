f45f08816b773ec0160393c47317effca6e9e636
Trương Công Phi <tcp02012003@gmail.com>
2023-11-16 20:19:21 +0700

    update

--- src/cau_hoi.php
+++ src/cau_hoi.php
@@ -1,73 +1,81 @@
-
-	<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đóng góp</title>
+    <title>Thêm câu hỏi</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <link rel="icon" type="image/x-icon" href="../images/logo6.png">    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="icon" type="image/x-icon" href="../images/logo6.png">
+    <link rel="stylesheet" href="../css/nav.css">
     <link rel="stylesheet" href="../css/footer.css">
 
     <!-- database table script cdn -->
     <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap4.min.js">
+    </script>
     <!-- database table script cdn -->
     <!-- database table css link cdn -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.bootstrap4.min.css">
     <!-- database table css link cdn -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="	sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="	sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <style>
-        main{
+        main {
             /*text-align: center;*/
             padding: 0 5%;
-            margin-top: 15%;
             overflow-x: auto;
             min-height: 80vh;
             font-family: 'Nunito', sans-serif;
         }
-        section{
+
+        section {
             width: 100%;
         }
-        td img{
+
+        td img {
             max-width: 650px;
         }
     </style>
 </head>
+
 <body>
-	
-	<?php include 'navbar.php';?>	
-    <style type="text/css">@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@500;700&display=swap');
-</style>
+
+    <?php include 'navbar.php';
+    include '../index.php'; ?>
+
+    <style type="text/css">
+        @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@500;700&display=swap');
+    </style>
 
     <main>
         <section>
-                        <div class="card border-dark mb-3" style="max-width: 100%;">
-                      <div class="card-header">Khóa học: Công nghệ Web</div>
-                      <div class="card-body text-dark">
-                        <h5 class="card-title">Chủ đề: Mảng và chuỗi</h5>
-                        <p class="card-text">Điểm cần đạt: 80</p>
-                      </div>
-                    </div>
+            <div class="card border-dark mb-3" style="max-width: 100%;">
+                <div class="card-header">Khóa học: Công nghệ Web</div>
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Chủ đề: Mảng và chuỗi</h5>
+                    <p class="card-text">Điểm cần đạt: 80</p>
+                </div>
+            </div>
 
 
             <div class="dropdown show">
                 <a class="btn btn-outline-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Thêm Câu hỏi
+                    Thêm Câu hỏi
                 </a>
                 <div class="dropdown-menu border-primary" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href='cau_hoi_mot_dap_an.php' target="">Dạng Singlechoice - 1 lựa chọn</a>
-                    <a class="dropdown-item" href='cau_hoi_nhieu_dap_an.php' target="">Dạng Multiplechoice - Chọn nhiều đáp án</a>
-                    <a class="dropdown-item" href='cau_hoi_dien.php' target="">Dạng Fill - Điền đáp án</a>
+                    <a class="dropdown-item" href='cau_hoi_mot_dap_an.php' target="">Câu hỏi 1 đáp án</a>
+                    <a class="dropdown-item" href='cau_hoi_nhieu_dap_an.php' target="">Câu hỏi nhiều đáp án
+                        đáp án</a>
+                    <a class="dropdown-item" href='cau_hoi_dien.php' target="">Câu hỏi điền</a>
                 </div>
             </div>
             <br><br>
@@ -76,75 +84,73 @@
             <div id="load_quiz">
                 <table style="width: 100%;" class="table table-striped table-bordered dt-responsive" id="myTable1">
                     <thead>
-                      <tr>
-                        <th title="Số thứ tự">STT</th>
-                        <th>Tên Quiz</th>
-                        <th>Loại Quiz</th>
-                        <th>Mức Độ</th>
-                        <th>Trạng thái</th>
-                        <th>Thao Tác</th>
-                      </tr>
+                        <tr>
+                            <th title="Số thứ tự">STT</th>
+                            <th>Tên Quiz</th>
+                            <th>Loại Quiz</th>
+                            <th>Mức Độ</th>
+                            <th>Trạng thái</th>
+                            <th>Thao Tác</th>
+                        </tr>
                     </thead>
                     <tbody>
-                                            </tbody>
+                    </tbody>
                 </table>
             </div>
 
-            
-            
-            
+
+
+
         </section>
     </main>
     <br><br><br>
     <style>
-	.footer-container{
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		min-height: 100px;
-		width: 100%;
-		padding: 15px;
-		/*background-color: #86b3f0;*/
-		/*background-color: #f8f9fa;*/
-		background-image: linear-gradient(180deg, hsla(214deg,100%,67%,.1),transparent 6rem);
-		flex-direction: column;
-	}
-	.footer-left{
-		display: flex;
-		align-items: center;
-		gap: 20px;
-	}
-	.footer-container p{
-		margin: 0 !important;
-		color: #737c83;
-		text-align: center;
-		/*font-weight: bold;*/
-		font-family: 'Nunito', sans-serif ;
+        .footer-container {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            min-height: 100px;
+            width: 100%;
+            padding: 15px;
+            /*background-color: #86b3f0;*/
+            /*background-color: #f8f9fa;*/
+            background-image: linear-gradient(180deg, hsla(214deg, 100%, 67%, .1), transparent 6rem);
+            flex-direction: column;
+        }
 
-	}
-	@media only screen and (max-width: 980px) {
-	  .footer-container {
+        .footer-left {
+            display: flex;
+            align-items: center;
+            gap: 20px;
+        }
 
-	  }
-	}
-</style>
-<footer>
-	<?php include'footer.php'?>
-</footer>
-<script>
-    $(document).ready(function () {
+        .footer-container p {
+            margin: 0 !important;
+            color: #737c83;
+            text-align: center;
+            /*font-weight: bold;*/
+            font-family: 'Nunito', sans-serif;
 
-        $('#myTable1').DataTable({
-            responsive: true
+        }
+
+        @media only screen and (max-width: 980px) {
+            .footer-container {}
+        }
+    </style>
+    <footer>
+        <?php include 'footer.php' ?>
+    </footer>
+    <script>
+        $(document).ready(function() {
+
+            $('#myTable1').DataTable({
+                responsive: true
+            });
+
+
         });
-       
-
-    });
-
-
-   
-
-</script>
+    </script>
 
 </body>
+
 </html>
--- src/khoa_hoc.php
+++ src/khoa_hoc.php
@@ -2,43 +2,42 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Khóa học</title>
-    <!-- Begin bootstrap cdn -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="	sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-    <!-- End bootstrap cdn -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Khóa học</title>
+	<!-- Begin bootstrap cdn -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="	sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+	</script>
+	<!-- End bootstrap cdn -->
 
 </head>
 
 <body>
-    <?php include 'navbar.php';?>
-    <main style="min-height: 100vh; width: 100%;">
-        <div class="" style="text-align: center;">
-            <h2>Khóa học</h2>
-        </div>
-        <div class="row row-cols-1 row-cols-md-3 g-4" style="margin: 0 auto; width: 80%;">
-            <!-- begin khóa học -->
+	<?php include 'navbar.php'; ?>
+	<main style="min-height: 100vh; width: 100%;">
+		<div class="" style="text-align: center;">
+			<h2>Khóa học</h2>
+		</div>
+		<div class="row row-cols-1 row-cols-md-3 g-4" style="margin: 0 auto; width: 80%;">
+			<!-- begin khóa học -->
 
-            <?php
+			<?php
 			include '../connectdb.php';
-			
-			
+			include '../index.php';
+
+
 			$username = $_SESSION['username'];
 
 			$query = "SELECT * FROM khoa_hoc";
 			$result = mysqli_query($conn, $query);
 
 			if (mysqli_num_rows($result) > 0) {
-			    while ($row = mysqli_fetch_assoc($result)) {
-			    	
-			    	if($username == 'admin'){
-			    		//admin: hiển thị tất cả khóa học
-			    		echo '<div class="col">
+				while ($row = mysqli_fetch_assoc($result)) {
+
+					if ($username == 'admin') {
+						//admin: hiển thị tất cả khóa học
+						echo '<div class="col">
 			    		        <div class="card">
 			    		            <img src="../images/khoahoc.jpg" class="card-img-top" alt="Course Image">
 			    		            <div class="card-body">
@@ -47,16 +46,15 @@
 			    		            </div>
 			    		        </div>
 			    		    </div>';
+					} else {
+						$id_khoa_hoc = $row["id_khoa_hoc"];
+						// kiểm tra xem người dùng đã đăng ký khóa học hay chưa
+						$query_check = "SELECT * FROM quan_ly_khoa_hoc WHERE id_khoa_hoc = $id_khoa_hoc AND id_username = '$username'";
+						$result_check = mysqli_query($conn, $query_check);
 
-			    	}else{
-			    		$id_khoa_hoc = $row["id_khoa_hoc"];
-			    		// kiểm tra xem người dùng đã đăng ký khóa học hay chưa
-			    		$query_check = "SELECT * FROM quan_ly_khoa_hoc WHERE id_khoa_hoc = $id_khoa_hoc AND id_username = '$username'";
-			    		$result_check = mysqli_query($conn, $query_check);
-
-			    		// nếu đã đăng ký thì hiển thị khóa học
-			    		if (mysqli_num_rows($result_check) > 0) {
-			    			echo '<div class="col">
+						// nếu đã đăng ký thì hiển thị khóa học
+						if (mysqli_num_rows($result_check) > 0) {
+							echo '<div class="col">
 			    			        <div class="card">
 			    			            <img src="../images/khoahoc.jpg" class="card-img-top" alt="Course Image">
 			    			            <div class="card-body">
@@ -65,20 +63,20 @@
 			    			            </div>
 			    			        </div>
 			    			    </div>';
-			    		}
-			    	}
-			    }
+						}
+					}
+				}
 			}
 			?>
 
 
 
-            <!-- end khóa học -->
+			<!-- end khóa học -->
 
 
-        </div>
-    </main>
-    <?php include 'footer.php'; ?>
+		</div>
+	</main>
+	<?php include 'footer.php'; ?>
 </body>
 
 
